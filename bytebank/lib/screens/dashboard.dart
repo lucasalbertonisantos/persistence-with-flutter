32642025d2276bbaf0cf3e5af0bd0cc4ef411baa
Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>
2020-08-24 17:50:27 -0300

    Atualizando o GestureDector para o InkWell envolvido pelo Material para ter um efeito de click. Signed-off-by: Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>

--- bytebank/lib/screens/dashboard.dart
+++ bytebank/lib/screens/dashboard.dart
@@ -20,34 +20,36 @@ class Dashboard extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => ContactsList(),
-                ));
-              },
-              child: Container(
-                padding: const EdgeInsets.all(8.0),
-                height: 100,
-                width: 150,
-                color: Theme.of(context).primaryColor,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Icon(
-                      Icons.people,
-                      color: Colors.white,
-                      size: 24.0,
-                    ),
-                    Text(
-                      'Contacts',
-                      style: TextStyle(
+            child: Material(
+              color: Theme.of(context).primaryColor,
+              child: InkWell(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => ContactsList(),
+                  ));
+                },
+                child: Container(
+                  padding: const EdgeInsets.all(8.0),
+                  height: 100,
+                  width: 150,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(
+                        Icons.people,
                         color: Colors.white,
-                        fontSize: 16.0,
+                        size: 24.0,
                       ),
-                    ),
-                  ],
+                      Text(
+                        'Contacts',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
