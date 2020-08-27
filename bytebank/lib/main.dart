b02be687075b2b8d8c1bd199c473385ffbb9e4ce
Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>
2020-08-26 22:38:54 -0300

    Deixando o card da lista de contatos dinâmico. Signed-off-by: Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,15 +1,7 @@
-import 'package:bytebank/database/app_database.dart';
-import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
-
-void main() {
-  runApp(MyApp());
-  save(Contact(0, 'lucas', 1000)).then((id){
-    findAll().then((contacts)=>debugPrint(contacts.toString()));
-  });
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
--- bytebank/lib/screens/contacts_list.dart
+++ bytebank/lib/screens/contacts_list.dart
@@ -1,27 +1,29 @@
 import 'dart:ffi';
 
+import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
+  final List<Contact> contacts = List();
+
   @override
   Widget build(BuildContext context) {
+    contacts.add(Contact(0,'Lucas',1000));
+    contacts.add(Contact(0,'Lucas',1000));
+    contacts.add(Contact(0,'Lucas',1000));
+    contacts.add(Contact(0,'Lucas',1000));
+    contacts.add(Contact(0,'Lucas',1000));
     return Scaffold(
       appBar: AppBar(
         title: Text('contacts'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              title: Text('Lucas'),
-              subtitle: Text(
-                '1000',
-                style: TextStyle(fontSize: 16.0),
-              ),
-            ),
-          )
-        ],
+      body: ListView.builder(
+        itemBuilder: (context, index) {
+          final contact = contacts[index];
+          return _ContactItem(contact);
+        },
+        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -39,6 +41,30 @@ class ContactsList extends StatelessWidget {
         },
         child: Icon(
           Icons.add,
+        ),
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: TextStyle(fontSize: 16.0),
         ),
       ),
     );
