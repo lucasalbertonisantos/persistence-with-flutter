fd6dd700572f5abaa0607f9bcdfc1217ff6b2d7f
Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>
2020-08-26 22:49:21 -0300

    Retornando a lista da base de dados utilizando o Future. Entretanto com o problema da espera da busca. Signed-off-by: Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>

--- bytebank/lib/screens/contacts_list.dart
+++ bytebank/lib/screens/contacts_list.dart
@@ -1,29 +1,30 @@
 import 'dart:ffi';
 
+import 'package:bytebank/database/app_database.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactsList extends StatelessWidget {
-  final List<Contact> contacts = List();
 
   @override
   Widget build(BuildContext context) {
-    contacts.add(Contact(0,'Lucas',1000));
-    contacts.add(Contact(0,'Lucas',1000));
-    contacts.add(Contact(0,'Lucas',1000));
-    contacts.add(Contact(0,'Lucas',1000));
-    contacts.add(Contact(0,'Lucas',1000));
     return Scaffold(
       appBar: AppBar(
         title: Text('contacts'),
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          final contact = contacts[index];
-          return _ContactItem(contact);
+      body: FutureBuilder(
+        future: findAll(),
+        builder: (context, snapshot) {
+          final List<Contact> contacts = snapshot.data;
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              final contact = contacts[index];
+              return _ContactItem(contact);
+            },
+            itemCount: contacts.length,
+          );
         },
-        itemCount: contacts.length,
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
