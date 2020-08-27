69fdbe622ac46b492adcc0ea3130d341b5d78199
Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>
2020-08-27 09:21:29 -0300

    Refatorando o método findAll para usar async await. Signed-off-by: Lucas Albertoni Santos <lucas.albertoni.santos@gmail.com>

--- bytebank/lib/database/app_database.dart
+++ bytebank/lib/database/app_database.dart
@@ -45,16 +45,28 @@ Future<int> save(Contact contact) async {
 //  });
 }
 
-Future<List<Contact>> findAll() {
-  return getDatabase().then((db) {
-    return db.query('contacts').then((maps) {
-      final List<Contact> contacts = List();
-      for (Map<String, dynamic> map in maps) {
-        final Contact contact =
-        Contact(map['id'], map['name'], map['account_number']);
-        contacts.add(contact);
-      }
-      return contacts;
-    });
-  });
+Future<List<Contact>> findAll() async {
+  final Database db = await getDatabase();
+  final List<Map<String, dynamic>> result = await db.query('contacts');
+  final List<Contact> contacts = List();
+  for (Map<String, dynamic> row in result) {
+    final Contact contact = Contact(
+      row['id'],
+      row['name'],
+      row['account_number'],
+    );
+    contacts.add(contact);
+  }
+  return contacts;
+//  return getDatabase().then((db) {
+//    return db.query('contacts').then((maps) {
+//      final List<Contact> contacts = List();
+//      for (Map<String, dynamic> map in maps) {
+//        final Contact contact =
+//        Contact(map['id'], map['name'], map['account_number']);
+//        contacts.add(contact);
+//      }
+//      return contacts;
+//    });
+//  });
 }
